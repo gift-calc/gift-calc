eb3c6271ab95ffe2eb5da4efb2e069ecb5a05d6c
David <david.nossebro@gmail.com>
2025-09-06 09:39:49 +0200

    Changed format for printing version

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function showVersion() {
     // Read package.json to get version
     const packageJsonPath = path.join(path.dirname(new URL(import.meta.url).pathname), 'package.json');
     const packageData = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
-    console.log(`gift-calc version ${packageData.version}`);
+    console.log(`Version: ${packageData.version}`);
   } catch (error) {
     console.error('Error reading version information');
     process.exit(1);
